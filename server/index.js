cf3d8e4c41069b210df53dc3ff21eb489c1dd627
Anugrah Kresnaya <anugrahkresnaya.ak@gmail.com>
2022-09-21 05:03:31 +0700

    feat: add a https server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,47 @@
-console.log("Implement servermu disini yak 😝!");
+const http = require('http');
+const { PORT = 8000 } = process.env; // Ambil port dari environment variable
+
+const fs = require('fs');
+const path = require('path');
+const PUBLIC_DIRECTORY = path.join(__dirname, '/../public');
+const CSS_DIRECTORY = path.join(__dirname, '/../public/css');
+const IMAGE_DIRECTORY = path.join(__dirname, '/../public/images');
+
+function getHTML(htmlFileName) {
+  const htmlFilePath = path.join(PUBLIC_DIRECTORY, htmlFileName);
+  return fs.readFileSync(htmlFilePath, 'utf-8')
+}
+
+function getCSS(cssFileName) {
+  const cssFilePath = path.join(CSS_DIRECTORY, cssFileName);
+  return fs.readFileSync(cssFilePath, 'utf-8');
+}
+
+function onRequest(req, res) {
+  if (req.url === "/") {
+    res.writeHead(200);
+    res.end(getHTML("index.html"))
+  } else if (req.url === "/cars") {
+    res.writeHead(200);
+    res.end(getHTML("cars.html"))
+  } else if (req.url.match("\.css$")) {
+    const cssPath = path.join(__dirname, '/../public', req.url);
+    const fileStream = fs.createReadStream(cssPath, "UTF-8");
+    res.writeHead(200, { "Content-Type": "text/css" });
+    fileStream.pipe(res);
+  } else if (req.url.match("\.png$")) {
+    const imagePath = path.join(__dirname, '/../public/', req.url);
+    const fileStream = fs.createReadStream(imagePath);
+    res.writeHead(200, { "Content-Type": "image/png" });
+    fileStream.pipe(res);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("404 | No Page Found");
+  }
+}
+
+const server = http.createServer(onRequest);
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log("server berjalan, silahkan buka http://0.0.0.0:%d", PORT);
+});
