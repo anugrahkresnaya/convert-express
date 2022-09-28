4690cb15752d54ae64915881c49bced3e11a7f95
Anugrah Kresnaya <anugrahkresnaya.ak@gmail.com>
2022-09-28 10:29:02 +0700

    refactor: change html content on ejs

--- server/index.js
+++ server/index.js
@@ -8,19 +8,20 @@ const PUBLIC_DIRECTORY = path.join(__dirname, '../public');
 app.use(express.static(PUBLIC_DIRECTORY));
 app.set('view engine', 'ejs');
 
-app.get("/", (res, req) => {
-  res.sendFile('index.html', { root: PUBLIC_DIRECTORY })
-});
+//app.get("/", (res, req) => {
+//  res.sendFile('index.html', { root: PUBLIC_DIRECTORY })
+//});
 
+//app.get("/cars", (req, res) => {
+//  res.sendFile('cars.html', { root: PUBLIC_DIRECTORY })
+//});
+
+app.get("/", (req, res) => {
+  res.render('index');
+});
 app.get("/cars", (req, res) => {
-  res.sendFile('cars.html', { root: PUBLIC_DIRECTORY })
-});
-
-app.get("/carsTest", (req, res) => {
-  res.render('index', {
-    name: 'asd'
-  })
-});
+  res.render('cars');
+})
 
 app.listen(port, () => {
   console.log(`Server running on http://0.0.0.0:${port}`)
